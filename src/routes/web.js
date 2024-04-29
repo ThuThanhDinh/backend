ea57d57f9793ac29f6ab3122897083c909f5c90d
Thanh Dinh <thuthanhdinh4@gmail.com>
2024-04-30 01:36:27 +0700

    nodejs v18

--- src/routes/web.js
+++ src/routes/web.js
@@ -16,8 +16,8 @@ const initWebRoutes = (app) => {
     router.get("/", homeController.handleHelloWorld)
     router.get("/user", homeController.handleUser)
     router.post("/users/create-user", homeController.handleCreateNewUser)
-    router.post("/delete-user/:id", homeController.handleDeleteUser)
-    router.post("/update-user/:id", homeController.handleUpdateUser)
+    // router.post("/delete-user/:id", homeController.handleDeleteUser)
+    // router.post("/update-user/:id", homeController.handleUpdateUser)
     return app.use("/", router);
 
 }
